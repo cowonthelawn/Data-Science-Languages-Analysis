efc31d1295ff228688d407c3b0ec2697b38521cf
Robert Allen <48074432+cowonthelawn@users.noreply.github.com>
2021-09-26 12:51:04 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,98 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from os.path import exists
+import process_survey_data
+import sys
+
+
+def main():
+    # load processed survey data
+    if exists(sys.path[0] + r'\data\processed_survey_data.csv'):
+        df_survey_combined = pd.read_csv(sys.path[0] + r'\data\processed_survey_data.csv')
+    else:
+        process_survey_data.main()
+        df_survey_combined = pd.read_csv(sys.path[0] + r'\data\processed_survey_data.csv')
+
+    # get overall count and the count of each language for each year
+    python_worked, python_want, r_worked, r_want, julia_worked, julia_want = ({} for i in range(6))
+    years = (2021, 2020, 2019, 2018, 2017)
+    for year in years:
+        df_curr_year = df_survey_combined[df_survey_combined["Year"] == year]
+        total_entries = df_curr_year["Year"].size
+        python_worked[year] = df_curr_year[df_curr_year["PythonWorkedWith"]]["PythonWorkedWith"].size / total_entries
+        r_worked[year] = df_curr_year["RWorkedWith"][df_curr_year["RWorkedWith"]].size / total_entries
+        julia_worked[year] = df_curr_year["JuliaWorkedWith"][df_curr_year["JuliaWorkedWith"]].size / total_entries
+        python_want[year] = df_curr_year["PythonWantWorkWith"][df_curr_year["PythonWantWorkWith"]].size / total_entries
+        r_want[year] = df_curr_year["RWantWorkWith"][df_curr_year["RWantWorkWith"]].size / total_entries
+        julia_want[year] = df_curr_year["JuliaWantWorkWith"][df_curr_year["JuliaWantWorkWith"]].size / total_entries
+
+    # plot a column chart of the count of each language    
+    print("Creating charts")
+    plt.style.use("ggplot")
+    num_column_groups = 3
+    counts_worked_2021 = (python_worked[2021], r_worked[2021], julia_worked[2021])
+    counts_want_2021 = (python_want[2021], r_want[2021], julia_want[2021])
+
+    column_group_pos = np.arange(num_column_groups)
+    width = 0.35
+    plt.bar(column_group_pos, counts_worked_2021, width, label="Worked with", color="steelblue")
+    plt.bar(column_group_pos + width, counts_want_2021, width, label="Want to work with", color="darkorange")
+
+    plt.xlabel("Language")
+    plt.ylabel("Percent of Developers")
+    plt.title("Use of and Interest in Data Science Languages in 2021")
+    plt.xticks(column_group_pos + width / 2, ("Python", "R", "Julia"))
+    plt.ylim(ymin=0, ymax=1)
+    plt.gca().set_yticklabels(['{:.0f}%'.format(x * 100) for x in plt.gca().get_yticks()])
+    plt.legend(loc="best")
+    plt.show()
+
+    # plot line chart showing growth of developers using Julia and wanting to use Julia
+    # missing 2019 data points for Julia
+    years = (2017, 2018, 2020, 2021)
+    worked_julia = (julia_worked[2017], julia_worked[2018], julia_worked[2020], julia_worked[2021])
+    want_julia = (julia_want[2017], julia_want[2018], julia_want[2020], julia_want[2021])
+    plt.plot(years, worked_julia, marker="o", label="Worked with Julia", color="steelblue")
+    plt.plot(years, want_julia, marker="o", label="Want to work with Julia", color="mediumseagreen")
+
+    plt.ylabel("Percent of Developers")
+    plt.title("Growth of Use and Interest in Julia")
+    plt.xticks((2017, 2018, 2019, 2020, 2021))
+    plt.ylim(ymin=0, ymax=0.18)
+    plt.gca().set_yticklabels(['{:.0f}%'.format(x*100) for x in plt.gca().get_yticks()])
+    plt.legend(loc="best")
+    plt.show()
+
+    # plot line chart showing growth of developers using and and wanting to use Python and R
+    python_color = "mediumseagreen"
+    r_color = "steelblue"
+    years = (2017, 2018, 2019, 2020, 2021)
+    worked_python = (python_worked[2017], python_worked[2018], python_worked[2019], python_worked[2020], python_worked[2021])
+    want_python = (python_want[2017], python_want[2018], python_want[2019], python_want[2020], python_want[2021])
+    worked_r = (r_worked[2017], r_worked[2018], r_worked[2019], r_worked[2020], r_worked[2021])
+    want_r = (r_want[2017], r_want[2018], r_want[2019], r_want[2020], r_want[2021])
+    plt.plot(years, worked_python, marker="o", linestyle="solid", color=python_color, label="Worked with Python")
+    plt.plot(years, want_python, marker="o", linestyle="dashed", color=python_color, label="Want to work with Python")
+    plt.plot(years, worked_r, marker="o", linestyle="solid", color=r_color, label="Worked with R")
+    plt.plot(years, want_r, marker="o",  linestyle="dashed", color=r_color, label="Want to work with R")
+
+    plt.ylabel("Percent of Developers")
+    plt.title("Growth of Use and Interest in Python and R")
+    plt.xticks((2017, 2018, 2019, 2020, 2021))
+    plt.ylim(ymin=0, ymax=1)
+    plt.gca().set_yticklabels(['{:.0f}%'.format(x*100) for x in plt.gca().get_yticks()])
+    plt.legend(loc="best")
+    plt.annotate("Python2 deprecated", xy=(2020, python_want[2020]), xytext=(-30, -30),
+                 textcoords='offset points', color=python_color,
+                 arrowprops=dict(arrowstyle="->", connectionstyle="angle3,angleA=0,angleB=-90", color=python_color))
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
---
+++ process_survey_data.py
@@ -0,0 +1,103 @@
+import pandas as pd
+import sys
+
+def rename_df_index_and_columns(mapping, df):
+    df_temp = df.copy()
+    df_temp.index.names = list(map(lambda name: mapping.get(name, name), df_temp.index.names))
+    return df_temp.rename(columns=mapping)
+
+
+def check_for_lang(lang, string_to_check):
+    return (lang.lower() + ";") in string_to_check.lower() \
+           or (";" + lang.lower()) in string_to_check.lower() \
+           or ("; " + lang.lower()) in string_to_check.lower() \
+           or string_to_check.lower() == lang.lower()
+
+
+def check_for_dev_types(string_to_check):
+    return "data scientist" in string_to_check.lower() \
+           or "machine learning" in string_to_check.lower() \
+           or "statistics" in string_to_check.lower()
+
+
+def main():
+    # import the survey results
+    print("Loading survey data")
+    df_survey_2021 = pd.read_csv(sys.path[0] + r'\data\developer_survey_2021\survey_results_public.csv')
+    df_survey_2020 = pd.read_csv(sys.path[0] + r'\data\developer_survey_2020\survey_results_public.csv')
+    df_survey_2019 = pd.read_csv(sys.path[0] + r'\data\developer_survey_2019\survey_results_public.csv')
+    # ambiguous dtypes in 2018 survey, handled by setting them all to object
+    df_survey_2018 = pd.read_csv(sys.path[0] + r'\data\developer_survey_2018\survey_results_public.csv',
+                                 dtype=object)
+    df_survey_2017 = pd.read_csv(sys.path[0] + r'\data\developer_survey_2017\survey_results_public.csv')
+
+    # add a year field to each dataframe
+    df_survey_2021["Year"] = 2021
+    df_survey_2020["Year"] = 2020
+    df_survey_2019["Year"] = 2019
+    df_survey_2018["Year"] = 2018
+    df_survey_2017["Year"] = 2017
+
+    # make a combined dataframe for all years
+    df_survey_combined = df_survey_2021[["Year", "DevType", "LanguageHaveWorkedWith", "LanguageWantToWorkWith"]].copy()
+
+    mapping = {"LanguageWorkedWith": "LanguageHaveWorkedWith", "LanguageDesireNextYear": "LanguageWantToWorkWith"}
+    df_survey_sliced = df_survey_2020[["Year", "DevType", "LanguageWorkedWith", "LanguageDesireNextYear"]].copy()
+    df_survey_sliced = rename_df_index_and_columns(mapping, df_survey_sliced)
+    df_survey_combined = df_survey_combined.append(df_survey_sliced)
+
+    df_survey_sliced = df_survey_2019[["Year", "DevType", "LanguageWorkedWith", "LanguageDesireNextYear"]].copy()
+    df_survey_sliced = rename_df_index_and_columns(mapping, df_survey_sliced)
+    df_survey_combined = df_survey_combined.append(df_survey_sliced)
+
+    mapping = {"LanguageWorkedWith": "LanguageHaveWorkedWith", "LanguageDesireNextYear": "LanguageWantToWorkWith"}
+    df_survey_sliced = df_survey_2018[["Year", "DevType", "LanguageWorkedWith", "LanguageDesireNextYear"]].copy()
+    df_survey_sliced = rename_df_index_and_columns(mapping, df_survey_sliced)
+    df_survey_combined = df_survey_combined.append(df_survey_sliced)
+
+    mapping = {"DeveloperType": "DevType", "HaveWorkedLanguage": "LanguageHaveWorkedWith",
+               "WantWorkLanguage": "LanguageWantToWorkWith"}
+    df_survey_sliced = df_survey_2017[["Year", "DeveloperType", "HaveWorkedLanguage", "WantWorkLanguage"]].copy()
+    df_survey_sliced = rename_df_index_and_columns(mapping, df_survey_sliced)
+    df_survey_combined = df_survey_combined.append(df_survey_sliced)
+
+    # reduce survey to only responses from data science related developers
+    series_data_scientist = df_survey_combined.apply(lambda x:
+                                                     True if check_for_dev_types(str(x["DevType"]))
+                                                     else False, axis=1)
+    df_survey_combined["IsDataScientist"] = series_data_scientist
+    df_survey_combined = df_survey_combined[df_survey_combined["IsDataScientist"]]
+
+    # engineer features determining if response indicated worked with applicable languages
+    series_python_worked = df_survey_combined.apply(lambda x:
+                                                    True if check_for_lang("Python", str(x["LanguageHaveWorkedWith"]))
+                                                    else False, axis=1)
+    series_r_worked = df_survey_combined.apply(lambda x:
+                                               True if check_for_lang("R", str(x["LanguageHaveWorkedWith"]))
+                                               else False, axis=1)
+    series_julia_worked = df_survey_combined.apply(lambda x:
+                                                   True if check_for_lang("Julia", str(x["LanguageHaveWorkedWith"]))
+                                                   else False, axis=1)
+    df_survey_combined["PythonWorkedWith"] = series_python_worked
+    df_survey_combined["RWorkedWith"] = series_r_worked
+    df_survey_combined["JuliaWorkedWith"] = series_julia_worked
+
+    print("Engineering new features")
+    # engineer features determining if response indicated wanted to work with applicable languages
+    series_python_want = df_survey_combined.apply(lambda x:
+                                                  True if check_for_lang("Python", str(x["LanguageWantToWorkWith"]))
+                                                  else False, axis=1)
+    series_r_want = df_survey_combined.apply(lambda x:
+                                             True if check_for_lang("R", str(x["LanguageWantToWorkWith"]))
+                                             else False, axis=1)
+    series_julia_want = df_survey_combined.apply(lambda x:
+                                                 True if check_for_lang("Julia", str(x["LanguageWantToWorkWith"]))
+                                                 else False, axis=1)
+    df_survey_combined["PythonWantWorkWith"] = series_python_want
+    df_survey_combined["RWantWorkWith"] = series_r_want
+    df_survey_combined["JuliaWantWorkWith"] = series_julia_want
+
+    # remove responses that did not list any languages
+    print("Cleaning data set")
+    df_survey_combined.dropna(subset=("LanguageHaveWorkedWith", "LanguageWantToWorkWith"), inplace=True)
+    df_survey_combined.to_csv(sys.path[0] + r'\data\processed_survey_data.csv')
